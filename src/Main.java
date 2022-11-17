91348c5cc82ca047a6ab09238a2d856e613186ae
Ernazar <shainazarov.er@gmail.com>
2022-11-18 02:28:31 +0600

    new jana

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Heloow world!");
         System.out.println("awrqwrfef");
+        System.out.println("a1213214");
     }
 }
